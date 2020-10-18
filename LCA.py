d47dcc6d4e7cfa108cf5ba828a0552610a4adf58
molnarbf <molnarb@tcd.ie>
2020-10-18 21:09:31 +0100

    findPath class added

--- LCA.py
+++ LCA.py
@@ -4,3 +4,12 @@ class Node:
         self.key = key
         self.left = None
         self.right = None
+# find path from root node to given root
+def findPath(root, path, k):
+    if root is None:
+        return False
+    
+    path.append(root.key)
+    if root.key is k:
+        return True
+    
