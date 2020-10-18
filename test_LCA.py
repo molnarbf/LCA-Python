725d3d8e1f7496f0cfd9720d5e34fc13994812ba
molnarbf <molnarb@tcd.ie>
2020-10-18 22:09:40 +0100

    another test for findPath function

--- test_LCA.py
+++ test_LCA.py
@@ -30,4 +30,20 @@ class TestLCA(unittest.TestCase):
         path = []
         self.assertEqual(findPath(root, path, 6), False)
 
-   
+    # test if node is in tree
+    def test_findPath(self):
+        path = []
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left(4)
+        root.left.right(5)
+        root.right.left(6)
+        root.right.right(7)
+
+        # node in tree
+        self.assertEqual(findPath(root, path, 2), True)
+
+        # node not in tree
+        self.assertEqual(findPath(root, path, 8), False)    
+
