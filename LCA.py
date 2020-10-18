e4c8f49dde2f0839a7ca3ce27949fc509b105b15
molnarbf <molnarb@tcd.ie>
2020-10-18 20:59:24 +0100

    Added Node class

---
+++ LCA.py
@@ -0,0 +1,6 @@
+# Python implementation for finding the Lowest Common Ancestor in a Binary Tree
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
