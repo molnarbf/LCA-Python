b2f5f5b3ab34b83fad9c11228ec5f667113e16c1
molnarbf <molnarb@tcd.ie>
2020-10-18 21:14:02 +0100

    findLCA class added

--- LCA.py
+++ LCA.py
@@ -4,6 +4,7 @@ class Node:
         self.key = key
         self.left = None
         self.right = None
+
 # find path from root node to given root
 def findPath(root, path, k):
     if root is None:
@@ -12,4 +13,13 @@ def findPath(root, path, k):
     path.append(root.key)
     if root.key is k:
         return True
+
+def findLCA(root, nr1, nr2):
+    path1 = []
+    path2 = []
+
+    # find path1 and path2 from root and return -1 if either node is not present
+    if (not findPath(root, path1, nr1) or not findPath(root, path2, nr2)): 
+        return -1 
+    
     
